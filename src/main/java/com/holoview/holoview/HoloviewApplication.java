e21c10efefdb4006679398586c59992b1e3e7196
KauanMO <kauaanmatheus@gmail.com>
2024-07-11 14:41:39 -0300

    feat: Estrutura inicial

---
+++ src/main/java/com/holoview/holoview/HoloviewApplication.java
@@ -0,0 +1,11 @@
+package com.holoview.holoview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoloviewApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HoloviewApplication.class, args);
+	}
+}
---
+++ src/test/java/com/holoview/holoview/HoloviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holoview.holoview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoloviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
