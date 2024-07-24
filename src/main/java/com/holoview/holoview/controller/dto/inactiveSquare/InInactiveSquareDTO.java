640bb96c1b059e3e6ac79228f89de714284155b9
KauanMO <kauaanmatheus@gmail.com>
2024-07-24 16:22:06 -0300

    feat: Aplicação de validation em dto de quadrados inativos

--- src/main/java/com/holoview/holoview/controller/dto/inactiveSquare/InInactiveSquareDTO.java
+++ src/main/java/com/holoview/holoview/controller/dto/inactiveSquare/InInactiveSquareDTO.java
@@ -2,6 +2,9 @@ package com.holoview.holoview.controller.dto.inactiveSquare;
 
 import java.util.UUID;
 
-public record InInactiveSquareDTO(Integer x, Integer y, UUID shopArrangementId) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record InInactiveSquareDTO(@Min(0) @NotNull Integer x, @Min(0) @NotNull Integer y, UUID shopArrangementId) {
     
 }
