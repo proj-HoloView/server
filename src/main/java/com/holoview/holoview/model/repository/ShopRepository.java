bf57bf9931584a097e0cc83d63a22f3eb021f9e4
KauanMO <kauaanmatheus@gmail.com>
2024-07-26 11:27:51 -0300

    fix: Busca de nome de loja

--- src/main/java/com/holoview/holoview/model/repository/ShopRepository.java
+++ src/main/java/com/holoview/holoview/model/repository/ShopRepository.java
@@ -1,5 +1,6 @@
 package com.holoview.holoview.model.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.holoview.holoview.model.entity.Shop;
 
 public interface ShopRepository extends JpaRepository<Shop, UUID> {
-
+    Optional<Shop> findByName(String name);
 }
