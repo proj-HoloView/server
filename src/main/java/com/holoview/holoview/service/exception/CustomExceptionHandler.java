6e953a545d4effaf3a22c379feb2795fef0b82f6
KauanMO <kauaanmatheus@gmail.com>
2024-08-19 12:42:40 -0300

    feat: Handlers de URI e argumentos incorretos

--- src/main/java/com/holoview/holoview/service/exception/CustomExceptionHandler.java
+++ src/main/java/com/holoview/holoview/service/exception/CustomExceptionHandler.java
@@ -1,6 +1,13 @@
 package com.holoview.holoview.service.exception;
 
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,11 +33,29 @@ public class CustomExceptionHandler {
         return ResponseEntity.badRequest().body(new OutError(e.getMessage()));
     }
 
+    @ExceptionHandler(URISyntaxException.class)
+    public ResponseEntity<?> uriSyntaxHandler(URISyntaxException e) {
+        return ResponseEntity.badRequest().body(new OutError(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidHandler(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        return ResponseEntity
+                .badRequest()
+                .body(new OutError("Erro nos campos: " +
+                        Arrays.toString(fieldErrors.toArray())));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> internalServerErrorHandler(Exception e) {
         System.out.println(e.toString());
         return ResponseEntity.internalServerError().body(e.getMessage());
     }
 
-    public record OutError(String message) { };
+    public record OutError(String message) {
+    };
 }
